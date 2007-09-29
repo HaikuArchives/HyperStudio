e408b67c159387d08ae0bf61612b4c63a3c7a89e
plfiorini <none@none>
2007-09-29 00:18:11 -0700

    [svn r16] Added initial (very unfinished) ScaleSliderView
    --HG--
    branch : trunk

---
+++ gui/project/ScaleSlider.cpp
@@ -0,0 +1,15 @@
+
+#include <Slider.h>
+
+#include "SliderView.h"
+
+SliderView::SliderView(BRect frame)
+	: BView(frame, "SliderView")
+{
+	fSlider = new BSlider(frame, "ScaleSlider", NULL,
+		new BMessage(kScaleChanged), 10, 100, B_TRIANGLE_THUMB);
+	fSlider->SetValue(DEFAULT_SCALE);
+	fSlider->SetHashMarks(B_HASH_MARKS_TOP);
+	fSlider->SetHashMarkCount(10);
+	AddChild(fSlider);
+}
---
+++ gui/project/ScaleSlider.h
@@ -0,0 +1,15 @@
+
+#ifndef SLIDER_VIEW_H
+#define SLIDER_VIEW_H
+
+#include <View.h>
+
+class BSlider;
+
+class SliderView : public BView
+{
+public:
+	SliderView(BRect frame);
+};
+
+#endif // SLIDER_VIEW_H
