23c066137398e21f0ac10c6d8674c5ae0200c000
plfiorini <none@none>
2007-10-04 07:01:50 -0700

    [svn r24] Better time line view.
    --HG--
    branch : trunk

--- beos/project/TimeLineView.cpp
+++ beos/project/TimeLineView.cpp
@@ -31,15 +31,11 @@ void
 TimeLineView::Draw(BRect updateRect)
 {
 	BRect r = Bounds();
-	rgb_color bg_color = (rgb_color){ 35, 70, 100 };
-	//rgb_color light_color = tint_color(bg_color, B_LIGHTEN_1_TINT);
 
 	// Fill with background color
+	rgb_color bg_color = (rgb_color){ 35, 70, 100 };
 	SetLowColor(bg_color);
 	FillRect(r, B_SOLID_LOW);
-
-	rgb_color white = (rgb_color){ 255, 255, 255 };
-	rgb_color small_color = tint_color(white, B_DARKEN_1_TINT);
 
 	// Get scale from project settings
 	ProjectView *parent = dynamic_cast<ProjectView*>(Parent());
@@ -54,37 +50,42 @@ TimeLineView::Draw(BRect updateRect)
 	if (nb <= 0)
 		return;
 
-	// Draw lines
+	// Change font size and get font height
+	SetFontSize(8.0f);
+	font_height fh;
+	GetFontHeight(&fh);
+
+	// Colors
+	rgb_color white = (rgb_color){ 255, 255, 255 };
+	rgb_color long_color = tint_color(white, B_DARKEN_2_TINT);
+	rgb_color small_color = tint_color(white, B_DARKEN_3_TINT);
+
+	// Set labels color
+	SetHighColor(white);
+
+	// Draw lines and labels
 	BeginLineArray(nb);
 	for (int32 i = start; i <= end; i += scale)
 	{
-		AddLine(BPoint(i, 0), BPoint(i, height), white);
+		// Draw lines
+		AddLine(BPoint(i, 0), BPoint(i, height), long_color);
 		AddLine(BPoint(i + scale / 2, height), BPoint(i + scale / 2, height - 5), small_color);
-	}
-	EndLineArray();
 
-	// Draw labels
-	SetHighColor(white);
-	for (int32 i = start; i <= end; i += scale)
-	{
+		// Draw labels
 		BString str;
-		int32 sec = i / scale;
-		if (!(sec % 5))
+		int32 val = i / scale;
+		uint32 min = uint32(val / 60);
+		uint32 sec = uint32(val % 60);
+		if (!(val % 5))
 		{
-			str << uint32(sec / 60);
-			str << ":";
-			str << uint32(sec % 60);
-			MovePenTo(i - StringWidth(str.String())/2, 20);
+			str << min << ":";
+			if (min > 0 && sec < 10)
+				str << "0" << sec;
+			else
+				str << sec;
+			MovePenTo(i - StringWidth(str.String()) / 2.0f, fh.ascent);
 			DrawString(str.String());
 		}
 	}
-
-// FIXME: don't know what is this, i took it from VBL
-rgb_color black = (rgb_color){255,0,0};
-	BeginLineArray(4);
-	AddLine(BPoint(updateRect.left, 30), BPoint(updateRect.right, 30), black);
-	AddLine(BPoint(updateRect.left, 60), BPoint(updateRect.right, 60), black);
-	AddLine(BPoint(updateRect.left, 80), BPoint(updateRect.right, 80), black);
-	AddLine(BPoint(updateRect.left, 110), BPoint(updateRect.right, 110), black);
 	EndLineArray();
 }
