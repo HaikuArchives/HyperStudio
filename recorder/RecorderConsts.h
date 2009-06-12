f9c3dfbff59400d1e42f417ef81addc5974c6190
plfiorini <none@none>
2009-06-12 04:41:28 -0700

    [svn r50] * Renamed HyperionConsts to RecorderConsts.h. * Updated license to GPL v3.
    --HG--
    branch : trunk
    rename : recorder/HyperionConsts.h => recorder/RecorderConsts.h

--- recorder/RecorderConsts.h
+++ recorder/RecorderConsts.h
@@ -1,13 +1,12 @@
 /*
- * Copyright 2007 Pier Luigi Fiorini. All rights reserved.
- * Distributed under the terms of the MIT License.
+ * Copyright 2007-2009 Pier Luigi Fiorini. All rights reserved.
+ * Distributed under the terms of the GPL v3 License.
  *
  * Authors:
  *		Pier Luigi Fiorini, pierluigi.fiorini@gmail.com
  */
-
-#ifndef HYPERION_CONSTS_H
-#define HYPERION_CONSTS_H
+#ifndef RECORDER_CONSTS_H
+#define RECORDER_CONSTS_H
 
 #include <SupportDefs.h>
 
@@ -25,4 +24,4 @@ const uint32 MENU_PROJECT_EXPORT       = 'MPEX';
 const uint32 MENU_TRACKS_ADDAUDIO      = 'MTAA';
 const uint32 MENU_TRACKS_ADDMIDI       = 'MTAM';
 
-#endif // HYPERION_CONSTS_H
+#endif // RECORDER_CONSTS_H
